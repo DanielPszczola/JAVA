2446af342961184f865c0142c8d89654ff17d1cf
DanielPszczola <102531727+DanielPszczola@users.noreply.github.com>
2023-06-08 19:59:09 +0200

    Delete JAVA-main/movieService/movieService directory

--- JAVA-main/movieService/movieService/src/main/java/pl/pjatk/movieService/Interface/MovieRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.pjatk.movieService.Interface;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.pjatk.movieService.model.Movie;
-
-public interface MovieRepository extends JpaRepository<Movie, Long> {
-}
