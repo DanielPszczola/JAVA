3c9f16cebb091bc0e8f161d235e7c2b3eb7f5469
DanielPszczola <102531727+DanielPszczola@users.noreply.github.com>
2023-06-25 20:26:08 +0200

    Delete nbpservice directory

--- nbpservice/src/main/java/pl/pjatk/jazs24310nbp/repository/CurrencyDataRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.pjatk.jazs24310nbp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.pjatk.jazs24310nbp.model.CurrencyData;
-
-public interface CurrencyDataRepository extends JpaRepository<CurrencyData, Long> {
-}
